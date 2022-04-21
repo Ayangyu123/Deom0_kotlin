7c964c1e8af8d12505dc846aef14c95f991009e1
Ayangyu123 <1819747216@qq.com>
2022-04-21 16:58:43 +0800

    '第三次提交更改了MainActivity'

--- app/src/main/java/com/gyp/deom0_kotlin/MainActivity.kt
+++ app/src/main/java/com/gyp/deom0_kotlin/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 我在MainActivity中添加一行注释
     }
 }
